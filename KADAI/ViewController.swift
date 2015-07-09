eefaeefabb161871e4c4c888182bde053368f7dd
Ryo Sakurai <ptolemaios923@icloud.com>
2015-07-09 23:27:54 +0900

    first commit

---
+++ KADAI/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  KADAI
+//
+//  Created by 桜井諒 on 2015/07/09.
+//  Copyright (c) 2015年 桜井諒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet var myLabel: UILabel!
+
+    @IBAction func one(sender: AnyObject) {
+    }
+    @IBAction func two(sender: AnyObject) {
+    }
+    @IBAction func three(sender: AnyObject) {
+    }
+    @IBAction func four(sender: AnyObject) {
+    }
+    
+}
+
