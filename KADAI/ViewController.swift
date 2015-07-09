905d873b108ccb7c8f98d7036718b106493be60c
Ryo Sakurai <ptolemaios923@icloud.com>
2015-07-10 04:01:20 +0900

    commit

--- KADAI/ViewController.swift
+++ KADAI/ViewController.swift
@@ -12,14 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        //ラベルに最初に表示する文字
+        myLabel.text = "(x,y,a,b)"
+        
+        //乱数発生
+        var myRundom = ["x","y","a","b"]
+        //整数の場合はIntを指定
+        var ran = 1 + Int(arc4random_uniform(4))
+
+        
+        //ラベルに表示
+        myLabel.text = myRundom[ran]
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     @IBOutlet var myLabel: UILabel!
+    
+    
 
     @IBAction func one(sender: AnyObject) {
     }
